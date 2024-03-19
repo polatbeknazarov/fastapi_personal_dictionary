2ced4032e63d1919982fdc5b1d5f26a5bc8a000e
polat <polatbekbeknazarov2003@gmail.com>
2024-03-19 23:16:42 +0500

    Added created and updated columns to base model

--- src/database.py
+++ src/database.py
@@ -1,22 +1,28 @@
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    mapped_column,
-    declared_attr,
+from datetime import datetime
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
 )
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from config import settings
 
 
-class Base(DeclarativeBase):
+class BaseMixin:
+    id: int = Column(Integer, primary_key=True)
+    created_at: datetime = Column(DateTime, default=datetime.utcnow)
+    updated_at: datetime = Column(DateTime, default=datetime.utcnow,
+                                  onupdate=datetime.utcnow)
+
+
+class Base(DeclarativeBase, BaseMixin):
     __abstract__ = True
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f'{cls.__name__.lower()}s'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
 
 
 class DataBase:
