b38ce9ecf10d0796755e49abae0f4151b0ae84ed
polat <polatbekbeknazarov2003@gmail.com>
2024-03-19 23:27:49 +0500

    Created users model

---
+++ src/auth/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Ema
+
+
+from database import Base
+
+
+class User(Base):
+    username: str = Column(String(50), unique=True, nullable=False)
+    first_name: str = Column(String(100), nullable=True)
+    last_name: str = Column(String(100), nullable=True)
+    email: str = Column(String(100), nullable=False, unique=True)
+    hashed_password: str = Column(String(128), nullable=False)
