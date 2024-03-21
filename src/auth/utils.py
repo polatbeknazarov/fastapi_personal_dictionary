77eeb99b02ef0d2ae4833635584713db04380c5c
polat <polatbekbeknazarov2003@gmail.com>
2024-03-21 18:06:12 +0500

    Added hash and verify password functions

---
+++ src/auth/utils.py
@@ -0,0 +1,9 @@
+from config import password_context
+
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_context.verify(password, hashed_password)
