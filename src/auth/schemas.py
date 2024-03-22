cf17cf6ceeb9d448d97e1f4e87cddf0bba246b27
polat <polatbekbeknazarov2003@gmail.com>
2024-03-23 01:38:35 +0500

    Changer user schemas

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -3,18 +3,15 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserSchema(BaseModel):
-    username: str
+    username: Optional[str]
     password: str
-    email: Optional[EmailStr] = None
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
 
 
 class UserCreate(BaseModel):
     username: str = Field(min_length=3, max_length=50)
     email: EmailStr
-    first_name: Optional[str] = Field(None, min_length=2, max_length=30)
-    last_name: Optional[str] = Field(None, min_length=2, max_length=30)
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     password: str = Field(min_length=7, max_length=128)
     password2: str = Field(min_length=7, max_length=128)
 
