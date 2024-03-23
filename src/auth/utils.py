86793846f44c38e2772f9f040785280f9fa2dfeb
polat <polatbekbeknazarov2003@gmail.com>
2024-03-23 21:02:32 +0500

    Added token type field for jwt token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -13,7 +13,7 @@ def verify_password(password: str, hashed_password: str) -> bool:
     return password_context.verify(password, hashed_password)
 
 
-def encode_jwt(
+def encode_access_jwt(
         payload: dict,
         private_key: str = settings.PRIVATE_KEY_PATH.read_text(),
         algorithm: str = settings.ALGORITHM,
@@ -25,6 +25,7 @@ def encode_jwt(
     expire = now + timedelta(minutes=expire_minutes)
 
     to_encode.update(
+        token_type='access_token',
         exp=expire,
         iat=now,
     )
@@ -35,11 +36,38 @@ def encode_jwt(
     return encoded
 
 
+def encode_refresh_jwt(
+    payload: dict,
+    private_key: str = settings.PRIVATE_KEY_PATH.read_text(),
+    algorithm: str = settings.ALGORITHM,
+    expire_minutes: int = settings.REFRESH_TOKEN_EXPIRE_MINUTES,
+):
+    to_encode = payload.copy()
+
+    now = datetime.utcnow()
+    expire = now + timedelta(days=expire_minutes)
+
+    to_encode.update(
+        token_type='refresh_token',
+        exp=expire,
+        iat=now,
+    )
+
+    encoded = jwt.encode(
+        payload=to_encode,
+        key=private_key,
+        algorithm=algorithm,
+    )
+
+    return encoded
+
+
 def decode_jwt(
-        token: str | bytes, 
-        publick_key: str = settings.PUBLICK_KEY_PATH.read_text(), 
+        token: str | bytes,
+        publick_key: str = settings.PUBLICK_KEY_PATH.read_text(),
         algorithm: str = settings.ALGORITHM,
 ):
     decoded = jwt.decode(jwt=token, key=publick_key, algorithms=[algorithm])
+    print('#############################decoded###########################', decoded)
 
     return decoded
