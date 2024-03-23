43890538bd0410aa568f0dc27616c47c372fb177
polat <polatbekbeknazarov2003@gmail.com>
2024-03-23 21:01:44 +0500

    Added refresh token route

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,6 +1,8 @@
 from fastapi import (
     APIRouter,
     Depends,
+    HTTPException,
+    status,
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,8 +13,8 @@ from auth.schemas import (
     Token,
     UserRead,
 )
-from auth.services import get_user, create_user
-from auth.utils import encode_jwt
+from auth.services import create_user, get_user
+from auth.utils import encode_access_jwt, encode_refresh_jwt, decode_jwt
 from auth.dependencies import (
     validate_auth_user,
     get_current_user,
@@ -44,12 +46,45 @@ async def login_user(user: UserSchema = Depends(validate_auth_user)):
     jwt_payload = {
         'sub': user.username,
     }
-    access = encode_jwt(jwt_payload)
+    access = encode_access_jwt(jwt_payload)
+    refresh = encode_refresh_jwt(jwt_payload)
 
     return Token(
         access_token=access,
+        refresh_token=refresh,
         token_type='Bearer',
     )
+
+
+@router.post('/token/refresh')
+async def refresh_access_token(refresh_token: str, session: AsyncSession = Depends(database.session_dependency),):
+    try:
+        payload = decode_jwt(token=refresh_token)
+        username: str = payload.get('sub')
+        print('##!@#!@#!@#!@#!@#!@#!@#@!#', username, payload.get('token_type'))
+
+        if username is None or payload.get('token_type') != 'refresh_token':
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid refresh token')
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid refresh token')
+
+    user = await get_user(session=session, username=username)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
+
+    jwt_payload = {
+        'sub': user.username,
+    }
+
+    access_token = encode_access_jwt(payload=jwt_payload)
+
+    return {
+        'access_token': access_token,
+    }
 
 
 @router.get('/me', response_model=UserRead)
