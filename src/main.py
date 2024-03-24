a6f1304a6bf9781e07728aab0bf23cdc978cb2ca
polat <polatbekbeknazarov2003@gmail.com>
2024-03-24 19:43:36 +0500

    Created words table

--- src/main.py
+++ src/main.py
@@ -5,19 +5,22 @@ from fastapi import FastAPI
 from database import Base, database
 from auth.routes import router as auth_router
 from auth.models import User
+from dictionary.routes import router as dictionary_router
+from dictionary.models import Word
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with database.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all, tables=[User.__table__,])
-        await conn.run_sync(Base.metadata.create_all, tables=[User.__table__,])
+        await conn.run_sync(Base.metadata.drop_all, tables=[User.__table__, Word.__table__,])
+        await conn.run_sync(Base.metadata.create_all, tables=[User.__table__, Word.__table__,])
 
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
+app.include_router(dictionary_router)
 
 
 if __name__ == '__main__':
