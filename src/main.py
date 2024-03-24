82b482e28b30cf8e2fdb6cf5fd28cdcb2fa25eae
polat <polatbekbeknazarov2003@gmail.com>
2024-03-24 21:55:45 +0500

    Added routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from database import Base, database
 from auth.routes import router as auth_router
@@ -12,15 +12,19 @@ from dictionary.models import Word
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with database.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all, tables=[User.__table__, Word.__table__,])
+        # await conn.run_sync(Base.metadata.drop_all, tables=[User.__table__, Word.__table__,])
         await conn.run_sync(Base.metadata.create_all, tables=[User.__table__, Word.__table__,])
 
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(auth_router)
-app.include_router(dictionary_router)
+
+router = APIRouter(prefix='/api/v1')
+router.include_router(auth_router)
+router.include_router(dictionary_router)
+
+app.include_router(router)
 
 
 if __name__ == '__main__':
