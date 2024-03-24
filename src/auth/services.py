2f018d9dda4ff46720719b9eb774b314aee3b1e8
polat <polatbekbeknazarov2003@gmail.com>
2024-03-24 21:55:54 +0500

    Code refactoring

--- src/auth/services.py
+++ src/auth/services.py
@@ -43,5 +43,4 @@ async def create_user(session: AsyncSession, user: UserCreate) -> User:
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=str(e)
         )
